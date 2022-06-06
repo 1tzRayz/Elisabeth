becf940dfc346f8b58f3b99a88e718411c739757
1tzRayz <106563842+1tzRayz@users.noreply.github.com>
2022-06-06 04:18:39 +0200

    migration to nextcord

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import discord
+import nextcord
 import os
 import levels
 import utils
 import users
 import LoL
-from discord.ext import commands
+from nextcord.ext import commands
 from webserver import keep_alive
 
 
@@ -15,7 +15,7 @@ bot_admin = 931454068033986560
 prefix = '-'
 
 
-client = commands.AutoShardedBot(command_prefix=prefix, intents = discord.Intents.all(), activity = discord.Activity(name=f'{prefix}help pour de l\'aide', type=5))
+client = commands.AutoShardedBot(command_prefix=prefix, intents = nextcord.Intents.all(), activity = nextcord.Activity(name=f'{prefix}help pour de l\'aide', type=5))
 
 client.remove_command("help")
 
@@ -23,7 +23,7 @@ client.remove_command("help")
 @client.event
 async def on_member_join(member):
   channel = client.get_channel(917896874671878155)
-  embed=discord.Embed(color=0X800808, title="— Bienvenue !")
+  embed=nextcord.Embed(color=0X800808, title="— Bienvenue !")
   embed.set_thumbnail(url=member.avatar_url)
   embed.add_field(name="<a:exc:933858691239788614> Voici quelques endroits à visiter <a:exc:933858691239788614> :", value="\n **<:vamp:964096437858230273> ╭・୨ <#923874457695703051>** \n **<:heartransfusion:961895222634221608> ┊・୨<#917897427648909422>**\n **<a:heartbounce:933858707907969114> ╰・୨ <#917897378445557770>**", inline=False)
   await channel.send(f"{member.mention} — <@&935167007412875335>", embed=embed)
@@ -44,13 +44,13 @@ async def snipe(ctx):
     except:
         contents,author, channel_name, time = client.sniped_messages[ctx.guild.id]
     try:
-        embed = discord.Embed(description=contents , color=0X800808, timestamp=time)
+        embed = nextcord.Embed(description=contents , color=0X800808, timestamp=time)
         embed.set_image(url=bob_proxy_url)
         embed.set_author(name=f"{author.name}#{author.discriminator}", icon_url=author.avatar_url)
         embed.set_footer(text=f"Supprimé dans : #{channel_name}")
         await ctx.channel.send(embed=embed)
     except:
-        embed = discord.Embed(description=contents , color=0X800808, timestamp=time)
+        embed = nextcord.Embed(description=contents , color=0X800808, timestamp=time)
         embed.set_author(name=f"{author.name}#{author.discriminator}", icon_url=author.avatar_url)
         embed.set_footer(text=f"Supprimé dans : #{channel_name}")
         await ctx.channel.send(embed=embed)
