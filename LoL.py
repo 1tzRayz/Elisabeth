6aefb38a63f0e31f7deb07853546d875167eac10
1tzRayz <106563842+1tzRayz@users.noreply.github.com>
2022-06-06 04:18:42 +0200

    migration to nextcord

--- LoL.py
+++ LoL.py
@@ -1,6 +1,6 @@
-import discord
+import nextcord
 import random
-from discord.ext import commands
+from nextcord.ext import commands
 chan_gaming = 917897748638990336
 
 champs=["Aatrox","Akali","Aurelion Sol","Ahri","Alistar","Amumu","Anivia","Annie","Ashe","Azir","Bard","Blitzcrank","Brand","Braum","Camille","Caitlyn","Cassiopeia","Cho Gath","Corki","Darius","Dr. Mundo","Diana","Draven","Ekko","Elise","Evelynn","Ezreal","Fiddlesticks","Fiora","Fizz","Galio","Gangplank","Garen","Gnar","Gragas","Graves","Hecarim","Heimerdinger","Irelia","Ivern","Illaoi","Janna","Jarvan IV","Jax","Jayce","Jhin","Jinx","Karma","Kalista","Karthus","Kassadin","Kai Sa","Katarina","Kayn","Kayle","Kennen","Kha Zix","Kog Maw","Kled","Kindred","LeBlanc","Lee Sin","Leona","Lucian","Lissandra","Lulu","Lux","Malphite","Malzahar","Maokai","Master Yi","Miss Fortune","Mordekaiser","Morgana","Nami","Nasus","Nautilus","Neeko","Nidalee","Nocturne","Nunu","Olaf","Orrn","Orianna","Pantheon","Poppy","Pyke","Quinn","Rammus","Rakan","Rek Sai","Renekton","Rengar","Riven","Rumble","Ryze","Sejuani","Shaco","Shen", "Shyvana", "Singed", "Sion", "Sivir", "Skarner", "Sona", "Soraka", "Swain", "Syndra", "Sylas", "Talon", "Taliyah","Taric","Tahm Kench","Thresh","Teemo","Tristana","Trundle","Tryndamere","Twisted Fate","Twitch","Udyr","Urgot","Varus","Vayne","Veigar","Vel Koz","Vi","Viktor","Vladimir","Volibear","Warwick","Wukong","Xayah","Xerath","Xin Zhao","Yasuo","Yorick","Zac","Zed","Ziggs","Zilean","Zoe","Zyra","Yuumi","Senna","Qiana","Sett","Aphelios","Lillia","Yone","Samira","Seraphine","Rell","Viego","Gwen","Akshan","Vex", "Zeri", "Renata"]
@@ -13,7 +13,7 @@ class LoL(commands.Cog):
     async def Whos(self, ctx):
       champion = random.choice(champs)
       if ctx.channel.id == chan_gaming:
-            embed=discord.Embed(color=0X800808, title=f"Qui est le meilleur joueur de {champion} ?")
+            embed=nextcord.Embed(color=0X800808, title=f"Qui est le meilleur joueur de {champion} ?")
             await ctx.send(embed=embed)
     
 def setup(client):
