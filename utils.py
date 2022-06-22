6140bd81228c83cb96b9f735c36373664bc7ad9f
1tzRayz <106563842+1tzRayz@users.noreply.github.com>
2022-06-22 20:28:58 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ from nextcord.ext import commands
 bot_channel = 922663411660251206
 bump_channel = 917897595190386710
 bot_admin = 931454068033986560
-prefix = '-'
+prefix = '+'
 
 class utils(commands.Cog):
     def __init__(self, client):
@@ -12,13 +12,16 @@ class utils(commands.Cog):
     @commands.command(aliases=['aide'])
     async def help(self, ctx):
         em = nextcord.Embed(title="Commandes d'Elisabeth",color=0x880808)
-        em.add_field(name="```-pic [membre]```", value="Permet de récupérer la photo de profil de quelqu'un.", inline=False)
-        em.add_field(name="```-banner [membre]```", value="Permet de récupérer la bannière de quelqu'un.", inline=False)
-        em.add_field(name="```-level [membre]```", value="Permet d'afficher des informations sur l'XP de quelqu'un.", inline=False)
-        em.add_field(name="```-profil [membre]```", value="Donne le profil de quelqu'un.", inline=False)
-        em.add_field(name="```-leaderboard / -lb```", value="Permet d'afficher le classement d'XP du serveur.", inline=False)
-        em.add_field(name="```-whos```", value="Demande qui est le meilleur joueur sur <champion de LoL>.", inline=False)
-        await ctx.send(embed=em)
+        em.add_field(name="```+pic [membre]```", value="Permet de récupérer la photo de profil de quelqu'un.", inline=False)
+        em.add_field(name="```+banner [membre]```", value="Permet de récupérer la bannière de quelqu'un.", inline=False)
+        em.add_field(name="```+level [membre]```", value="Permet d'afficher des informations sur l'XP de quelqu'un.", inline=False)
+        em.add_field(name="```+profil [membre]```", value="Donne le profil de quelqu'un.", inline=False)
+        em.add_field(name="```+leaderboard | -lb```", value="Permet d'afficher le classement d'XP du serveur.", inline=False)
+        em.add_field(name="```+ban | +unban [membre]```", value="Permet de bannir ou de débannir un membre.", inline=False)
+        em.add_field(name="```+lock | +unlock```", value="Permet de lock ou unlock un channel.", inline=False)
+        em.add_field(name="```-kick [membre]```", value="Permet de kick un membre.", inline=False)
+        if ctx.channel.id == bot_channel:
+          await ctx.send(embed=em)
       
    
 def setup(client):
