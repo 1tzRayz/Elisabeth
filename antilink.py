91882d2f26cffecce7aa7a81a63cb72adc550d3d
1tzRayz <106563842+1tzRayz@users.noreply.github.com>
2022-07-09 18:38:19 +0200

    Update antilink.py

--- antilink.py
+++ antilink.py
@@ -7,12 +7,13 @@ class antilink(commands.Cog):
         
      
     @commands.Cog.listener()
-    async def on_message(self, message):
+    async def on_message(self, ctx, message):
         x = self.client.get_channel(995091597509279804)
         if "discord.gg" in message.content.lower():
-            await message.delete()
-            await message.channel.send("Tu n'as pas le droit d'envoyer d'invitation sur ce serveur.")
             await x.send(f"{message.author.mention} à envoyé une invitation dans le salon {message.channel.mention}.")
+            await message.delete()
+            await ctx.channel.send("Tu n'as pas le droit d'envoyer d'invitation sur ce serveur.")
+            
         
 
 def setup(client):
